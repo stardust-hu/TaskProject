44faced968dd211dcf1e672daf0bea67879df80b
yuhao <yuhao.hu1992@gmail.com>
2017-03-17 23:37:50 +0800

    1. add login 2. add show table

--- TaskCheck/urls.py
+++ TaskCheck/urls.py
@@ -1,9 +1,10 @@
 # coding: utf8
 from django.conf.urls import url
 
-from .views import index, show_person_task_list
+from .views import login, index, show_person_task_list
 
 urlpatterns = [
     url(r'^$', index, name='home'),
+    url(r'^login/', login, name='login'),
     url(r'^PersonTaskList/', show_person_task_list, name='person_task_list'),
 ]
--- TaskCheck/views.py
+++ TaskCheck/views.py
@@ -4,6 +4,10 @@ from .models import PersonTask, Person, Tag, Task, DetailPost, PersonTaskList
 from .forms import AddPersonTask
 
 
+def login(request):
+    return render(request, template_name="TaskCheck/login.html")
+
+
 def index(request):
     user = request.user
     if not user.is_authenticated():
@@ -15,21 +19,37 @@ def index(request):
     return render(request, template_name="TaskCheck/index.html", context={'user': user, 'form': form})
 
 
+def get_checked_list(task_checked_list):
+    checked_list = dict()
+    for task in task_checked_list:
+        checked_list[task.task.task] = {
+            'is_checked': task.is_solve,
+            'url': task.url,
+        }
+    return checked_list
+
+
 def show_person_task_list(request):
     context = {'date': '2017-03-17'}
     if request.method == 'POST':
         date = request.POST['date']
         today_total_task_list = PersonTaskList.objects.filter(date=date, person__person__username=request.user.username)
         today_task_checked_list = PersonTask.objects.filter(date=date, person__person__username=request.user.username)
-        print(today_task_checked_list)
-        if len(today_total_task_list) > 1:
+        checked_list = get_checked_list(today_task_checked_list)
+        if len(today_total_task_list) != 1:
             print('错误，请联系管理员')
         else:
+            show_table = []
             today_total_task_list = today_total_task_list[0]
-            print(today_total_task_list.date, today_total_task_list.person)
             total_task = today_total_task_list.task.all()
             for each_task in total_task:
-                print(each_task.task, each_task.describe)
+                each_row = [None, None, None, None]
+                each_row[0] = each_task.task
+                each_row[1] = checked_list[each_task.task]['is_checked']
+                each_row[2] = checked_list[each_task.task]['url']
+                each_row[3] = each_task.describe
+                show_table.append(each_row)
+            context['show_table'] = show_table
     else:
         pass
     return render(request, template_name="TaskCheck/person_task_list.html", context=context)
